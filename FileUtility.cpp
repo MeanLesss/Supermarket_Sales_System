3c173786237a2d680a1aa4e6e69ed3c2df4ee815
MeanLesss <68323176+MeanLesss@users.noreply.github.com>
2021-11-15 02:29:04 +0700

    done

--- FileUtility.cpp
+++ FileUtility.cpp
@@ -4,8 +4,8 @@
 #include<vector>
 #include"AccountUser.cpp"
 #include"CartProduct.cpp"
-
-
+#include"Cashier.cpp"
+#include"Manager.cpp"
 using namespace std;
 
 class FileIO
@@ -13,6 +13,8 @@ class FileIO
 private:
     ofstream fout;
     ifstream fin;
+
+    int count = 0;
 public:
     void OpenDataToFile(string filename)//OpenDataToFile(string filename)
     {
@@ -34,16 +36,136 @@ public:
         }
         fin.seekg(0, ios::beg);
     }
+
     void WriteDataToFileUser(AccountUser account)//save it to void WriteDataTooFile(AccountUser & account)
     {
         fout.write(reinterpret_cast<char*>(&account),sizeof(AccountUser));
     }
 
-    void ReadDataFromFileUser(AccountUser & account)
+    void ReadDataFromFileUser(AccountUser & account,string role)
     {
-       fin.read(reinterpret_cast<char*>(&account),sizeof(AccountUser));
+        Manager manager;
+        Cashier cashier;
+        count = 0;
+        if (role == "manager")
+        {
+            while (1)
+            {
+                fin.read(reinterpret_cast<char*>(&manager), sizeof(AccountUser));
+                if (fin.eof()) { break; }
+                if (manager.getRole() == role)
+                {
+                    manager.DisplayManager();
+                    cout << endl;
+                }
+
+            }
+        }
+        else if (role == "cashier")
+        {
+            while (1)// I use this method because it read better than the function read in while(argument)
+            {
+                fin.read(reinterpret_cast<char*>(&cashier), sizeof(AccountUser));
+                if (fin.eof()) { break; }
+
+                if (cashier.getRole() == role)
+                {
+                    cashier.DisplayCashier();
+                    cout << endl;
+                }
+
+            }
+        }
     }
-  
+
+    void WriteUpdateUser(AccountUser account, string username)
+    {
+        count = 0;
+        fin.seekg(0, ios::beg);
+        while (fin.read(reinterpret_cast<char*>(&account), sizeof(AccountUser)))
+        {
+            if (fin.eof()) { break; }
+            if (account.getUsername() != username)
+            {
+                WriteDataToFileUser(account);
+            }
+            else
+            {
+                account.CreateUser(2);
+                WriteDataToFileUser(account);
+                count++;
+            }
+        }
+        if (count == 0)
+        {
+            cout << "Account not found" << endl;
+        }
+    }
+
+    void WriteDeleteUser(AccountUser account, string username)
+    {
+        count = 0;
+        while (fin.read(reinterpret_cast<char*>(&account), sizeof(AccountUser)))
+        {
+            if (fin.eof()) { break; }
+            if (account.getUsername() != username)
+            {
+                WriteDataToFileUser(account);
+            }
+            else
+            {
+                count++;
+            }
+        }
+        if (count == 0)
+        {
+            cout << "\t\t\t\tAccount not found" << endl;
+        }
+        else
+        {
+            cout << "\t\t\t\tUser deleted" << endl;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    void RenameRemoveUser()
+    {
+        int res = 0;
+        if (remove("user.dat") != 0)
+        {
+            cout << "Failed to remove file!" << endl;
+        }
+        //remove(USER_FILE);
+        res = rename("temp.dat", "user.dat");
+        if (res == 0)
+        {
+            cout << "Succeed!" << endl;
+        }
+        else
+        {
+            cout << "Failed to rename file" << endl;
+        }
+    }
+
+
+
+
+
     void WriteDataToFileReport(vector<CartProduct> storingProduct)
     {
         unsigned int size = storingProduct.size();
--- Service.cpp
+++ Service.cpp
@@ -44,170 +44,50 @@ public:
 	//void LoadFromUser(string role)
 	void LoadFromUser(string role)
 	{
-		int count = 0;
+		AccountUser account;
 		ifstream fin;
-		fin.open(USER_FILE, ios::in | ios::binary);
-		
-		if (!fin) 
-		{
-			cerr << "File open failed" << endl;
-		}
-		else
-		{
-			if (role == "manager")
-			{
-				while (1)
-				{
-					fin.read(reinterpret_cast<char*>(&manager), sizeof(AccountUser));
-					if (fin.eof()) { break; }
-					if (manager.getRole() == role)
-					{
-						manager.DisplayManager();
-						cout << endl;
-					}
-					
-				}
-			}
-			else if (role == "cashier")
-			{
-				while (1)// I use this method because it read better than the function read in while(argument)
-				{
-					fin.read(reinterpret_cast<char*>(&cashier), sizeof(AccountUser));
-					if (fin.eof()) { break; }
 
-					if (cashier.getRole() == role)
-					{
-						cashier.DisplayCashier();
-						cout << endl;
-					}
-					
-				}
-			}
-			
-		}
-		fin.close();
+		fileIO.OpenDataFromFile(USER_FILE);
+
+		fileIO.ReadDataFromFileUser(account, role);
+
+		fileIO.CloseDataFromFile();
 	}
 
 	void DeleteUser(string username)
 	{
-		int count = 0; //account found detector
 		AccountUser account;
-		ifstream fin;
-		ofstream fout;
 
 		fileIO.OpenDataToFile("temp.dat");
-		fin.open(USER_FILE, ios::in | ios::binary);
 
-		if (!fin)
-		{
-			cerr << "File open failed";
-			exit(1);
-		}
-		while (fin.read(reinterpret_cast<char*>(&account), sizeof(AccountUser)))
-		{
-			if (fin.eof()) { break; }
-			if (account.getUsername() != username)
-			{
-				fileIO.WriteDataToFileUser(account);
-			}
-			else
-			{
-				count++;
-			}
-		}
-		if (count == 0)
-		{
-			cout << "\t\t\t\tAccount not found" << endl;
-		}
-		else
-		{
-			cout << "\t\t\t\tUser deleted" << endl;
-		}
+		fileIO.OpenDataFromFile(USER_FILE);
 
-		fin.close();
+		fileIO.WriteDeleteUser(account, username);
+		
+		fileIO.CloseDataFromFile();
+
 		fileIO.CloseDataToFile();
 
-		int res = 0;
-		if (remove("user.dat") != 0)
-		{
-			cout << "Failed to remove!" << endl;
-		}
-		//remove(USER_FILE);
-		res = rename("temp.dat","user.dat");
-		if (res == 0)
-		{
-			cout << "File deleted" << endl;
-		}
-		else
-		{
-			cout << "Failed to rename" << endl;
-		}
+		fileIO.RenameRemoveUser();
 		
 	}
 
 	void UpdateUser(string username)
 	{
-		int count = 0;
 		AccountUser account;
-		ifstream fin;
 
 		fileIO.OpenDataToFile("temp.dat");
-		fin.open(USER_FILE, ios::in | ios::binary);
 
-		if (!fin)
-		{
-			cerr << "File open failed";
-			exit(1);
-		}
-		fin.seekg(0, ios::beg);
-		while (fin.read(reinterpret_cast<char*>(&account), sizeof(AccountUser)))
-		{
-			if (fin.eof()) { break; }
-			if (account.getUsername() != username)
-			{
-				fileIO.WriteDataToFileUser(account);
-			}
-			else
-			{
-				account.CreateUser(2);
-				fileIO.WriteDataToFileUser(account);
-				count++;
-			}
-		}
-		if (count == 0)
-		{
-			cout << "Account not found" << endl;
-		}
-		fin.close();
+		fileIO.OpenDataFromFile(USER_FILE);
+
+		fileIO.WriteUpdateUser(account,username);
+
+		fileIO.CloseDataFromFile();
+
 		fileIO.CloseDataToFile();
-		int res = 0;
-		if (remove("user.dat") != 0)
-		{
-			cout << "Failed to remove!" << endl;
-		}
-		//remove(USER_FILE);
-		res = rename("temp.dat", "user.dat");
-		if (res == 0)
-		{
-			cout << "\t\t\t\tUser updated!" << endl;
-		}
-		else
-		{
-			cout << "Failed to rename" << endl;
-		}
-	}
 
-	// Load report of each cashier 
-	void SaveReportFile()
-	{
+		fileIO.RenameRemoveUser();
 
 	}
-
-	void LoadReportFile()
-	{
-
-	}
-
-
 
 };
